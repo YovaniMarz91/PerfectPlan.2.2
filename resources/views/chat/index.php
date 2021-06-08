4747425843f49cd1a55b1d5cc5f547dc90235885
Yovani Ulises Chinchilla Martinez <69612480+YovaniMarz91@users.noreply.github.com>
2021-06-07 23:12:28 -0500

    diseño2

--- resources/views/chat/index.php
+++ resources/views/chat/index.php
@@ -3,7 +3,7 @@
 <head><!-- Define la cabecera del documento -->
     <meta charset="UTF-8"><!-- Definimos la codificacion del documento  -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenidos a Progra IV</title><!-- Titulo de la aplicacion en la ventana del navegador -->
+    <title>Bienvenidos a PerfectPlan</title><!-- Titulo de la aplicacion en la ventana del navegador -->
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
@@ -109,11 +109,11 @@
         </div>
         <div class="col-lg-10">
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <label for="exampleFormControlInput1" class="form-label">Nombre Usuario</label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <label for="exampleFormControlTextarea1" class="form-label"></label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
           </div>
         </div>
